d768b9a8266a57286331fcf1e6c8665c737f9de9
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-06-10 16:59:22 +0800

    完善注释

--- 自己编的.js
+++ 自己编的.js
@@ -1,45 +1,41 @@
 /**
  * 目前可以实现自动刷新读取题库
  * 待实现功能
- * 1、题库map无变化时停止递归调用
+ * 1、题库map无变化时停止递归调用(已实现，待测试)
  * 2、导出题库为json文件
  * 
  */
-
 var time = 5;
 let examinationQuestions = new Map();
 var href = window.location.href;
 let eqCount = 0;
+let eqSize = 0;
 // let d = {};
 if (time > 0) {
     setTimeout(reload, 1000 * time)
 }
 function reload() {
     var fram = '<frameset col="*"><frame src="' + href + '"/></frameset>'
-
     with (document) {
         write(fram)
         void (close())
     }
     iframe = document.querySelector("frame");
-    iframe.onload = function() {
-
+    iframe.onload = function () {
         console.log('Iframe content has fully loaded.');
-        // console.log(document.querySelector("frame").contentDocument.querySelectorAll(".field.ui-field-contain"));
-        // 可以在这里执行需要在iframe加载完成后进行的操作
-        Array.from(document.querySelector("frame").contentDocument.querySelectorAll(".field.ui-field-contain")).forEach(function(item) {
-
-            // d.topic = item.getAttribute("topic");
-            // d.title = item.textContent;
-            examinationQuestions.set(item.getAttribute("topic"),item.textContent);
+        // frame加载完成查找所有考题元素，并遍历填入map。
+        Array.from(document.querySelector("frame").contentDocument.querySelectorAll(".field.ui-field-contain")).forEach(function (item) {
+            examinationQuestions.set(Number(item.getAttribute("topic")), item.textContent);
         });
-
+        //连续三次题库map的size没有变化则退出调用
+        if (eqCount == 3) {
+            return false
+        }
+        if (eqSize == examinationQuestions.size) {
+            console.log(eqCount);
+            eqCount++;
+        }
+        eqSize = examinationQuestions.size
+        setTimeout(reload, 1000 * time)
     }
-    // console.log(document.readyState);
-    if (eqCount == 10) {
-        return false
-    }
-    console.log(eqCount);
-    eqCount++;
-    setTimeout(reload, 1000 * time)
 }
