e13e4a066ccdb18b463d20c592550b4e4402acb0
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-06-25 10:56:37 +0800

    公共机构节能宣传

---
+++ 2025天津市公共机构节能宣传周知识竞答.js
@@ -0,0 +1,59 @@
+/**基本信息 */
+document.querySelector("#q1_0").textContent="王奕";
+document.querySelector("#q1_1").textContent="天津市赤龙房地产开发有限公司";
+document.querySelector("#q1_2").textContent="15902209730";
+document.querySelector("#q1_3").textContent="天津市津南区双港镇久隆街168号双港镇党群服务中心";
+/**单选题 */
+document.querySelector("#div2 > div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div3 > div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div4 > div.ui-controlgroup.column1 > div:nth-child(1)> div").click();
+document.querySelector("#div5> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div6 > div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div7 > div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div8 > div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div9 > div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div10 > div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div11 > div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div12> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div13 > div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div14 > div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div15 > div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div16> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div17> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+
+/**多选题 */
+document.querySelector("#div18> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div18> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div18> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div19> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div19> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div19> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div19> div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div20> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div20> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div20> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div21> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div21> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div21> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div21> div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div22> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div22> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div22> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div23> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div23> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div23> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div23> div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div24> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div24> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div24> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div25> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div25> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div25> div.ui-controlgroup.column1 > div:nth-child(3) > div").click();
+document.querySelector("#div25> div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+document.querySelector("#div26> div.ui-controlgroup.column1 > div:nth-child(1) > div").click();
+document.querySelector("#div26> div.ui-controlgroup.column1 > div:nth-child(2) > div").click();
+document.querySelector("#div26> div.ui-controlgroup.column1 > div:nth-child(4) > div").click();
+/**提交 */
+var element=document.querySelector("#ctlNext");
+element.scrollIntoView({ behavior: 'smooth' });
+setTimeout("element.click()",15000)
